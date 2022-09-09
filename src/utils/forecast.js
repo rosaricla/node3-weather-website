b9a93a0a24648fdc8610370b54c4dc33057d6829
rosaricla <rbarcelona@rosaLGgram.localdomain>
2022-09-09 15:48:07 +0200

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to connect to find location.', undefined)
         } else{
-            const my_forecast = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.'
+            const my_forecast = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + "%."
             callback(undefined , my_forecast)
         }
     })
